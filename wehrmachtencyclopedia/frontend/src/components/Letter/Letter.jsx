e280ff137376aa5de72b2232df9542c722237c89
kerem37mert <kerem37mert@yahoo.com>
2025-07-22 11:00:38 +0300

    güncelleme

--- wehrmachtencyclopedia/frontend/src/components/Letter/Letter.jsx
+++ wehrmachtencyclopedia/frontend/src/components/Letter/Letter.jsx
@@ -7,16 +7,8 @@ const Letter = () => {
         <div className={ styles["letter-container"] }>
             <h3 className={ styles.letter }>A</h3>
             <Row>
-                <p className={ styles.word }>Lutwaffe:</p>
-                <p className={ styles.desc }>Hava Kuvvetleri</p>
-            </Row>
-            <Row>
-                <p className={ styles.word }>Anchluss:</p>
-                <p className={ styles.desc }>Nazi Almanyasının Avusturya topraklarını kendisine katması. Avusturyayı ilhak etmesi</p>
-            </Row>
-            <Row>
-                <p className={ styles.word }>sdjfhhjdshjkds gjksdfgıh:</p>
-                <p className={ styles.desc }>Hava Kuvvetleri</p>
+                <p className={ styles.term }>Lutwaffe:</p>
+                <p className={ styles.defination }>Hava Kuvvetleri</p>
             </Row>
         </div>
     );
