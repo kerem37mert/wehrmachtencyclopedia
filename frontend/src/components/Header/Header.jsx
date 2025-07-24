b80c78c19368b962256fa0b2b09fedc78cd5b0ae
kerem37mert <kerem37mert@yahoo.com>
2025-07-24 11:18:15 +0300

    Generaller sayfası oluşturuldu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,24 @@
 import styles from "./Header.module.css";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+    const [config, setConfig] = useState({});
+
+    useEffect(() => {
+        fetch("http://localhost:8000/api/config",
+            {
+                method: "GET"
+            })
+            .then(res => res.json())
+            .then(data => setConfig(data))
+            .catch(err => console.log(err))
+    }, []);
+
     return(
         <div className={ styles.header }>
-            <h1 className={ styles.title } >WEHRMACHT ANSİKLOPEDİSİ</h1>
-            <p className={ styles.subtitle } >Nazi ALmanyası Silahlı Kuvvetleri</p>
+            <h1 className={ styles.title } >{ config.title }</h1>
+            <p className={ styles.subtitle } >{ config.sub_title }</p>
         </div>
     );
 }
---
+++ frontend/src/pages/Generals/Generals.jsx
@@ -0,0 +1,8 @@
+const Generals = () => {
+    return(
+        <>
+        </>
+    );
+}
+
+export default Generals;
