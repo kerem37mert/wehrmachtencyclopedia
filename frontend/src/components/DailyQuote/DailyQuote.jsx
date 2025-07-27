b3991b0b2397de7dc631240c18a98b29f8648a11
kerem37mert <kerem37mert@yahoo.com>
2025-07-27 16:59:28 +0300

    quote tasarımı ve api isteği tamamlandı

--- frontend/src/components/DailyQuote/DailyQuote.jsx
+++ frontend/src/components/DailyQuote/DailyQuote.jsx
@@ -1,8 +1,24 @@
 import styles from "./DailyQuote.module.css";
+import { useEffect, useState } from "react";
 
 const DailyQuote = () => {
+
+    const [quote, setQuote] = useState({});
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_BACKEND_URL}/api/quote`, {
+            method: "GET",
+        })
+            .then(res => res.json())
+            .then(data => setQuote(data))
+            .catch(err => console.log(err));
+    }, []);
+
     return (
-        <div className={ styles.container }>asfas</div>
+        <div className={ styles.container }>
+            <q><em>{ quote.quote }</em></q>
+            <p className={styles.author}>{ quote.author?.String }</p>
+        </div>
     );
 }
 
