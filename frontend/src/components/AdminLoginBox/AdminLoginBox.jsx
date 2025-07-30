61557c1cbaf7fd516e2e55c829ead136fb7c2dbc
kerem37mert <kerem37mert@yahoo.com>
2025-07-30 15:13:05 +0300

    frontend trafıda çeitli düzenlemeler

--- frontend/src/components/AdminLoginBox/AdminLoginBox.jsx
+++ frontend/src/components/AdminLoginBox/AdminLoginBox.jsx
@@ -1,8 +1,26 @@
 import styles from "./AdminLoginBox.module.css";
+import {useState} from "react";
 
 const AdminLoginBox = () => {
+
+    const [username, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <div className={ styles["login-box"] }>sgsdgs</div>
+        <div className={ styles["login-box"] }>
+            <input
+                type="text"
+                placeholder="Kullancı Adı"
+                onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Parola"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button>Giriş Yap</button>
+            <div className={ styles.message }>Lütfen giriş bilgilerinizi kontrol ediniz</div>
+        </div>
     );
 }
 
