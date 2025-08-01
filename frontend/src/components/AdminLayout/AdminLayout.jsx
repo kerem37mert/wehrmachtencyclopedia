70e3ae31a8a083387171962d25b78c6ee5c34367
kerem37mert <kerem37mert@yahoo.com>
2025-08-01 18:21:13 +0300

    adminlayout güncellendi

--- frontend/src/components/AdminLayout/AdminLayout.jsx
+++ frontend/src/components/AdminLayout/AdminLayout.jsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router";
+import { Navigate, Outlet } from "react-router";
+import { useContext } from "react";
+import { AdminAuthContext } from "../../context/AdminAuthContext.js";
 
 const AdminLayout = () => {
+
+    const { isAuthenticated } = useContext(AdminAuthContext);
+
+    if(!isAuthenticated)
+        return <Navigate to="/giris" replace />
+
     return (
         <>
             <Outlet />
